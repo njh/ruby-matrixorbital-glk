9662451b7d3cc21d27096641468dcd073ec4f89c
Nicholas J Humfrey <njh@aelius.com>
2008-07-20 14:53:50 +0000

    renamed usb driver loading script

--- examples/load-driver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Load serial port driver for the GLK19264-7T-1U
-#
-
-sudo modprobe ftdi_sio vendor=0x1b3d product=0x0127
-
---
+++ examples/load-usb-driver.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Linux Script to load USB serial port driver for the GLK19264-7T-1U
+#
+
+sudo modprobe ftdi_sio vendor=0x1b3d product=0x0127
